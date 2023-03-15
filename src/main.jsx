4a0d87fbcb49db6ab847c2cc19d2ba634b0e75fe
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-03-15 16:49:51 +0700

    Refactor code (#5)
    * Remove demo pages and add room reservation pages
    
    * CI: Containerize application
    
    * CI: Add Azure DevOps pipeline
    
    * Update service to use Ingress instead of LoadBalancer
    
    * Chore: refactor rooms into 1 file

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import SM1 from "./routes/sm1";
-import SM2 from "./routes/sm2";
-import BM1 from "./routes/bm1";
-import BM2 from "./routes/bm2";
-import TR1 from "./routes/tr1";
+import { SM1, SM2, BM1, BM2, TR1 } from './routes/rooms';
 
-import Root from "./routes/root";
+import Root from './routes/root';
 
-import Index from "./routes/index";
+import Index from './routes/index';
 
-import ErrorPage from "./error-page";
+import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Index /> },
       {
-        path: "sm1",
+        path: 'sm1',
         element: <SM1 />,
       },
       {
-        path: "sm2",
+        path: 'sm2',
         element: <SM2 />,
       },
       {
-        path: "bm1",
+        path: 'bm1',
         element: <BM1 />,
       },
       {
-        path: "bm2",
+        path: 'bm2',
         element: <BM2 />,
       },
       {
-        path: "tr1",
+        path: 'tr1',
         element: <TR1 />,
       },
     ],
-  }
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
