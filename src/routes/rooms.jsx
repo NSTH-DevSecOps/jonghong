b9cdeee9fae81b096df543094202e88367d279f2
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-03-20 18:41:35 +0700

    Make front-end fetch events stores in database from back-end

--- src/routes/rooms.jsx
+++ src/routes/rooms.jsx
@@ -1,55 +1,43 @@
 import { Scheduler } from '@aldabil/react-scheduler';
 
+/**
+ * Reconstruct event object as date object stores as text in database
+ * @param {JSON} event Event object created from '@aldabil/react-scheduler'
+ * @returns event
+ */
+function eventReconstruct(event) {
+  event.start = new Date(event.start);
+  event.end = new Date(event.end);
+  return event;
+}
+
 export function SM1() {
+  const fetchRemote = async (query) => {
+    var events = await fetch(
+      'http://localhost:8080/api/events'
+    ).then((response) => response.json());
+    await events.forEach((event) => (event = eventReconstruct(event)));
+    return events;
+  };
+
   return (
-    <>
-      <Scheduler
-        view='month'
-        timeZone='Asia/Bangkok'
-        month={{
-          startHour: 9,
-          endHour: 18,
-        }}
-        week={{
-          startHour: 9,
-          endHour: 18,
-        }}
-        day={{
-          startHour: 9,
-          endHour: 18,
-        }}
-        fields={[
-          {
-            name: 'user_id',
-            type: 'select',
-            // Should provide options with type:"select"
-            options: [
-              { id: 1, text: 'John', value: 1 },
-              { id: 2, text: 'Mark', value: 2 },
-            ],
-            config: {
-              label: 'User',
-              required: true,
-              errMsg: 'Plz Select User',
-            },
-          },
-          {
-            name: 'Description',
-            type: 'input',
-            default: 'Default Value...',
-            config: { label: 'Details', multiline: true, rows: 4 },
-          },
-        ]}
-        events={[
-          {
-            event_id: 1,
-            title: 'Event 1 Room 1',
-            start: new Date('2023/3/1 09:00'),
-            end: new Date('2023/3/2 17:00'),
-          },
-        ]}
-      />
-    </>
+    <Scheduler
+      view='month'
+      timeZone='Asia/Bangkok'
+      month={{
+        startHour: 9,
+        endHour: 18,
+      }}
+      week={{
+        startHour: 9,
+        endHour: 18,
+      }}
+      day={{
+        startHour: 9,
+        endHour: 18,
+      }}
+      getRemoteEvents={fetchRemote}
+    />
   );
 }
 
