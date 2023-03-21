c8fc05d621424974e8b2fc58469d07c9ccbcf89c
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-03-21 23:36:09 +0700

    Change picture on error page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,17 +1,26 @@
 import { useRouteError } from "react-router-dom";
 
+import Box from "@mui/material/Box";
+
+import Typography from "@mui/material/Typography";
+
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <Box sx={{ textAlign: "center" }}>
+      <Typography variant="h5" component="div">
+        {" "}
+        Oops!
+      </Typography>
+      <Typography variant="subtitle2" component="div">
+        Sorry, an unexpected error has occurred.
+      </Typography>
+      <Typography variant="subtitle1" component="div">
         <i>{error.statusText || error.message}</i>
-      </p>
-      <img src="https://http.cat/404.jpg"></img>
-    </div>
+      </Typography>
+      <img src="https://http.cat/418.jpg"></img>
+    </Box>
   );
 }
