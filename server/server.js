f53842281f9f54b1b3a4c313647bfb838a2493b2
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-03-23 14:22:07 +0700

    CI: Fix OpenShift deployment

--- server/server.js
+++ server/server.js
@@ -1,43 +1,51 @@
-// const compression = require('compression');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+// const compression = require('compression');
 // const helmet = require('helmet');
 
 const dbConnector = require('./db/dbConnector.js');
 const api_router = require('./routes/api_router.js');
 
 async function createServer() {
-	const app = express();
+  const app = express();
 
-	// app.use(compression());
-	app.use(bodyParser.json());
-	app.use(morgan('combined'));
-	app.use(cors({
-		origin: true
-	}));
-	// app.use(helmet());
-	
-    await dbConnector.connect(String(process.env.MONGODB_URI));
-
-	app.get('/healthz', (req, res) => {
-        res.status(200).send('OK');
+  // app.use(compression());
+  app.use(bodyParser.json());
+  app.use(
+    cors({
+      origin: [
+		'https://jonghong.nsth.net',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'OPTIONS', 'DELETE'],
     })
+  );
+  // app.use(helmet());
 
-	// API Routes
-	app.use('/api', api_router);
+  app.use(morgan('combined'));
 
-	// Start Server
-	let server = await app.listen(process.env.PORT, () => console.log('App listen on port: ' + process.env.PORT));
+  await dbConnector.connect(String(process.env.MONGODB_URI));
 
-	process.on('SIGINT', () => {
-		console.log('Application Terminated!')
-		server.close((err) => {
-			console.log(err)
-			process.exit(1)
-		})
-	})
+  app.get('/healthz', (req, res) => {
+    res.status(200).send('OK');
+  });
+
+  // API Routes
+  app.use('/api', api_router);
+
+  // Start Server
+  let server = await app.listen(process.env.PORT, () =>
+    console.log('App listen on port: ' + process.env.PORT)
+  );
+
+  process.on('SIGINT', () => {
+    console.log('Application Terminated!');
+    server.close((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+  });
 }
 
 createServer();
