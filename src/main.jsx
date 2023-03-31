fc35c438ce54a9f6b087a5f44d38f8659729c05e
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2023-04-01 01:56:52 +0700

    24 refactor to spa (#25)
    * CI: Fix sed command to correct file for frontend
    
    * Feature!: Refactor to SPA #24
    
    - Refactor frontend to SPA
    - Remove react-router and other unused dependencies
    - Remove /rooms API from backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import * as ReactDOMClient from 'react-dom/client';
 
-import { SM1, SM2, BM1, BM2, TR1 } from './routes/rooms';
+import App from "./App";
 
-import Root from './routes/root';
-
-import Index from './routes/index';
-
-import ErrorPage from './error-page';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Index /> },
-      {
-        path: 'sm1',
-        element: <SM1 />,
-      },
-      {
-        path: 'sm2',
-        element: <SM2 />,
-      },
-      {
-        path: 'bm1',
-        element: <BM1 />,
-      },
-      {
-        path: 'bm2',
-        element: <BM2 />,
-      },
-      {
-        path: 'tr1',
-        element: <TR1 />,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
+root.render(<App />);
